c1edc3633008cfd5fcccdfebdece1cf414d89da2
Johnny Dallas <j@zeet.co>
2023-10-09 01:01:38 -0400

    generate circle masks, and empty circles for falsey bits

--- cmd/framer/main.go
+++ cmd/framer/main.go
@@ -22,10 +22,17 @@ func convertToBinary(input int) string {
 
 func generateImage(binaryString string) (*gg.Context, error) {
 	fmt.Printf("generating image for string: %s\n", binaryString)
-	const imageSize = 256
+	const (
+		imageSize = 256
+		lineWidth = 2.0
+	)
+
+	radius := imageSize / gridSize / 2
+
 	dc := gg.NewContext(imageSize, imageSize)
 	dc.SetRGB(0, 0, 0)
 	dc.Clear()
+	dc.SetLineWidth(lineWidth)
 
 	structuredString := [bitCount]rune{}
 	for i, b := range binaryString {
@@ -37,12 +44,20 @@ func generateImage(binaryString string) (*gg.Context, error) {
 		y := math.Floor(float64(i) / gridSize)
 		x := i - (y * gridSize)
 
+		geoY := y + radius*imageSize/gridSize
+		geoX := x + radius*imageSize/gridSize
+
+		fmt.Printf("drawing bit #%f at (%f, %f)\n", i, x, y)
+		dc.SetRGB(1, 1, 1)
+
 		bit := structuredString[int(i)]
 		if bit == '1' {
-			fmt.Printf("drawing bit #%f at (%f, %f)\n", i, x, y)
-			dc.SetRGB(1, 1, 1)
-			dc.DrawRectangle(x*imageSize/gridSize, y*imageSize/gridSize, imageSize/gridSize, imageSize/gridSize)
+			dc.DrawCircle(geoX, geoY, radius)
+			// dc.DrawRectangle(x*imageSize/gridSize, y*imageSize/gridSize, imageSize/gridSize, imageSize/gridSize)
 			dc.Fill()
+		} else {
+			dc.DrawEllipse(geoX, geoY, radius, radius)
+			dc.Stroke()
 		}
 
 	}
